de1cdc6adadb2d5bad2d262f0d61074588439a16
Daniel <sicmylife@gmail.com>
2022-06-25 16:28:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+
+import './App.css';
+import freecodecamplogo from './images/freecodecamp-logo.png'
+import Boton from './componentes/Boton'
+import Contador from './componentes/contador'
+
+import { useState } from 'react'
+
+
+function App() {
+
+  const [numClics,setnumClics] = useState(0)
+
+  
+
+ const manejarClic = () => {
+    setnumClics(numClics + 1);
+ }
+
+ const reiniciarContador = () => {
+  setnumClics(0);
+ }
+
+
+  return (
+    <div className="App">
+    <div className='freecodecamp-logo-contenedor'>
+      <img 
+      className='freecodecamp-logo'
+      src={freecodecamplogo}
+       alt="logo de freecodecamp" />
+    </div>
+
+          <div className="contenedor-principal">
+              <Contador numClics={numClics} />
+            <Boton
+             texto='Clic'
+             esBotonDeClic={true}
+             manejarClic={manejarClic} />
+               <Boton
+             texto='Reiniciar'
+             esBotonDeClic={false}
+             manejarClic={reiniciarContador} />
+          </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/contador.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../hojas-de-estilos/contador.css'
+
+function Contador({ numClics }) {
+    return(
+        <div 
+        className="contador">
+            {numClics}
+        </div>
+    );
+}
+
+export default Contador;
